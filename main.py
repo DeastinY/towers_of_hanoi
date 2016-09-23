28a7bbf4e9f649cb45a86716add816b653825bdf
DeastinY <polzin.richard@gmail.com>
2016-09-23 09:48:05 +0000

    Updated and cleaned the code. Added comments and added namedtuples

--- main.py
+++ main.py
@@ -25,60 +25,73 @@ MDP:
 
     The discount factor of future rewards is 0.9
     The problem will be solved using both Value Iteration and Policy Iteration.
+
+Implementation:
+    Take care, there are a few hickups:
+        1. Actions described as an initial and final state
 """
 import logging
 from copy import deepcopy
 import itertools
+from collections import namedtuple
 
 logging.basicConfig(level=logging.INFO)
+Action = namedtuple('Action', 'InitialState FinalState')
+Transition = namedtuple('Transition', 'Action Probability')
 
 
 def gen_actions(state, disk=None):
+    """Generates all possible actions (=resulting states) from a base state.
+    If disk is not None only action that move disk are returned."""
     if state[-1] == [2, 1]:
         return []
-    moves = []
+    actions = []
     for idx_out, pin in enumerate(state):
         for idx, move_to in enumerate(state):
             if pin != move_to and len(pin) != 0 and (disk is None or pin[-1] == disk):
-                new_move = deepcopy(state)
-                del new_move[idx_out][-1]
-                new_move[idx].append(pin[-1])
-                moves.append(new_move)
-    return moves
+                new_state = deepcopy(state)
+                del new_state[idx_out][-1]
+                new_state[idx].append(pin[-1])
+                actions.append(Action(state, new_state))
+    return actions
 
 
-def get_disk(state, action):
-    for i in range(len(state)):
-        if state[i] != action[i]:
-            d = set(state[i]).symmetric_difference(set(action[i]))
+def get_disk(action):
+    """return the disk that is moved from in the transition from state to action."""
+    for i in range(len(action.InitialState)):
+        if action.InitialState[i] != action.FinalState[i]:
+            d = set(action.InitialState[i]).symmetric_difference(set(action.FinalState[i]))
             logging.debug("Moving disk {}".format(d))
             return d.pop()
 
 
-def r(state, action):
-    for pin in action:
+def r(action):
+    for pin in action.FinalState:
         if not all(pin[i] >= pin[i + 1] for i in range(len(pin) - 1)):
             return -10
     else:
-        if action[-1] == [2, 1]:  # bad hardcoded win condition, fix later (adjust field size)
+        if action.FinalState[-1] == [2, 1]:  # bad hardcoded win condition, fix later (adjust field size)
             return 100
         else:
             return -1
 
 
-def t(state, action):
-    if state[-1] == [2, 1]:
+def t(action):
+    """Returns the possible transitions trying to execute an action on a state.
+    Base on the task the desired state s' has a 90% success rate. Every different state has an equal probability."""
+    if action.FinalState[-1] == [2, 1]:
         return []
-    disk = get_disk(state, action)
+    disk = get_disk(action)
     transitions = []
-    actions = gen_actions(state, disk)
+    actions = gen_actions(action.InitialState, disk)
     for g_a in actions:
-        p = 0.9 if g_a == action else 0.1/(len(actions)-1)
-        transitions.append((g_a, p))
+        p = 0.9 if g_a.FinalState == action.FinalState else 0.1/(len(actions)-1)
+        transitions.append(Transition(g_a, p))
     return transitions
 
 
 def u(state, updated_utility=None):
+    """Calculates or updates the current utility for a state. If no utility is present it is initialized to 0."""
     global best_utility
     for us in best_utility:
         if state in us:
@@ -93,7 +106,8 @@ def u(state, updated_utility=None):
 best_utility = []
 
 def unique(iterable):
-    # http://stackoverflow.com/questions/6534430/why-does-pythons-itertools-permutations-contain-duplicates-when-the-original
+    """Used to adjust the itertool permutations method to our needs.
+    http://stackoverflow.com/questions/6534430/why-does-pythons-itertools-permutations-contain-duplicates-when-the-original"""
     seen = set()
     for i in iterable:
         if str(i) in seen:
@@ -101,48 +115,43 @@ def unique(iterable):
         seen.add(str(i))
         yield i
 
-# generate all states - by hand, because life sucks and then you die
-states = [i for i in unique(itertools.permutations([[2,1],[],[]]))] + \
-         [i for i in unique(itertools.permutations([[2],[1],[]]))] + \
-         [i for i in unique(itertools.permutations([[1,2],[],[]]))]
 
-def value_iteration(epsilon):
+def value_iteration(epsilon, states):
     iterations = 0
     while True:
         iterations += 1
-        states_delta = []
-        states_best = []
+        states_delta, states_best = [], []
         for s in states:
             possible_actions = gen_actions(s)
             if len(possible_actions) == 0:
                 continue  # skip terminal states
-            state_utilities = []
-            state_moves = []
+            u_a_mapping = {}
             for a in possible_actions:
-                possible_transitions = t(s, a)
-                reward = sum([tr[1] * r(s, tr[0]) for tr in possible_transitions])
-                utility = reward + 0.9 * sum([tr[1] * u(tr[0]) for tr in possible_transitions])
-                state_utilities.append(utility)
-                state_moves.append(a)
-                logging.info("Investigating state {} and action {}".format(s, a))
-                [logging.debug("p: {}\t r: {}\t s': {}\t".format(
-                    tr[1], r(s, tr[0]), tr[0])) for tr in possible_transitions]
-                logging.debug("Reward for action : {}".format(reward))
-                logging.debug("Utility for action : {}".format(utility))
+                possible_transitions = t(a)
+                utility = r(a) + 0.9 * sum([tr.Probability * u(tr.Action.FinalState) for tr in possible_transitions])
+                u_a_mapping[utility] = a
 
-            idx = state_utilities.index(max(state_utilities))
-            new = state_utilities[idx]
-            move = state_moves[idx]
-            u(s, new)
-            logging.info("Updated utility {} to {} for {}".format(u(s), new, s))
-            logging.info("Current best move for this state is {}".format(move))
-            states_delta.append(abs(u(s)-new))
-            states_best.append((s, move, new))
+            best_u = max(u_a_mapping, key=int)
+            best_a = u_a_mapping[best_u]
+            u(s, best_u)
+            states_delta.append(abs(u(s)-best_u))
+            states_best.append((best_a, best_u))
         if all([d < epsilon for d in states_delta]):
             logging.info("Finished after {} iterations!".format(iterations))
             for b in states_best:
-                logging.info("For state {} action {} is best with utility {}".format(*b))
+                logging.info("For State {} moving to State {} is best, with utility {}".format(b[0].InitialState, b[0].FinalState, b[1]))
             return
 
-value_iteration(0.001)
+def policy_iteration(epsilon, states):
+    pass
+
+
+if __name__ == "__main__":
+    states = [i for i in unique(itertools.permutations([[2,1],[],[]]))] + \
+             [i for i in unique(itertools.permutations([[2],[1],[]]))] + \
+             [i for i in unique(itertools.permutations([[1,2],[],[]]))]
+
+    value_iteration(0.000001, states)
+    policy_iteration(0.000001, states)
+
 
